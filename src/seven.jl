85c6abe9ab63a1057bc577630d26af043fa1a072
NoFishLikeIan <andreatitton96@gmail.com>
2020-12-23 12:15:19 +0100

    🎅 part one of seventh day

---
+++ src/seven.jl
@@ -0,0 +1,41 @@
+rawrules = readlines("src/files/dayseven.txt")
+
+singular(cl) = rstrip(cl, 's')
+
+function parserule(rule)
+    color, contains = split(rstrip(rule, '.'), " contain ")
+
+    if contains == "no other bags"
+        return (color, Dict())
+    end
+
+    inner = split.(split(contains, ", "), " "; limit=2)
+
+    subbags = Dict((singular(color), parse(Int, count)) for (count, color) in inner)
+
+    return (singular(color), subbags)
+end
+
+function bagpaths!(currentset::Set, bagrules::Dict, lookfor::AbstractString)
+
+    hasbag = Set(
+        color for (color, contains) in bagrules if lookfor ∈ Set(keys(contains)))
+
+    union!(currentset, hasbag)
+
+    for bag ∈ hasbag bagpaths!(currentset, bagrules, bag) end
+end
+
+countin(bagrules::Dict, findinside::AbstractString) = sum(count * countin(bagrules, color) for (color, count) in get(bagrules, findinside, Dict())) # TODO: Nesting not working
+
+
+bagrules = Dict(parserule.(rawrules))
+
+paths = Set{AbstractString}()
+bagpaths!(paths, bagrules, "shiny gold bag")
+total = length(paths)
+
+nestcount = 0
+countin(bagrules, "shiny gold bag")
+
+
